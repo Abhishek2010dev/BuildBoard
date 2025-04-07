af1701570b858afe3cec981aad02adfc0f1986c6
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-04-07 18:36:25 +0530

    sideline component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import FloatingDockDemo from "@/components/navbar";
+import Sideline from "@/components/Sideline";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <FloatingDockDemo />
+          <Sideline />
           {children}
         </ThemeProvider>
       </body>
---
+++ components/Sideline.tsx
@@ -0,0 +1,12 @@
+export default function Sideline() {
+	return (
+		<>
+			<div className="absolute inset-y-0 left-0 h-full w-px bg-neutral-200/80 dark:bg-neutral-800/80">
+				<div className="absolute top-0 h-40 w-px bg-gradient-to-b from-transparent via-blue-500 to-transparent" />
+			</div>
+			<div className="absolute inset-y-0 right-0 h-full w-px bg-neutral-200/80 dark:bg-neutral-800/80">
+				<div className="absolute h-40 w-px bg-gradient-to-b from-transparent via-blue-500 to-transparent" />
+			</div>
+		</>
+	);
+}
