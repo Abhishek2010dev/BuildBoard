54383cd2e2be55a7646cbbe8ca82b3a038de1d28
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-04-08 21:16:40 +0530

    using section tag

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,9 +3,13 @@ import Information from "@/components/information";
 
 export default function About() {
   return (
-    <div className="flex flex-col md:flex-row gap-8 items-start w-full max-w-6xl mx-auto px-4 pt-24">
-      <AvatarComponent />
-      <Information />
-    </div>
+    <section className="flex flex-col md:flex-row gap-8 items-start w-full max-w-6xl mx-auto px-4 pt-24">
+      <section>
+        <AvatarComponent />
+      </section>
+      <section>
+        <Information />
+      </section>
+    </section>
   );
 }
